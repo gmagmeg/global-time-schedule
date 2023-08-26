8322a4358ca2f39061062a9de82573a00bf08f17
tsuchida_nt <mamameto3071@gmail.com>
2023-08-26 17:01:11 +0900

    サーバーコンポーネントと、chakra-uiを利用したクライアントコンポーネントの併用を確認した

---
+++ app/schedule/time-input-box.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { FC } from 'react'
+
+export const TimeInputBox = () => {
+
+  return (
+    <h1>クライアントコンポーネント</h1>
+  )
+}
--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,11 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './Button';
+import { TimeInputBox } from '../app/schedule/time-input-box';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Example/Button',
-  component: Button,
+  title: 'Example/Buttons',
+  component: TimeInputBox,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
