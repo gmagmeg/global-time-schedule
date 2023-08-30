ac67d42d43afce85842d5bbffcc11f479b74ba92
tsuchida_nt <mamameto3071@gmail.com>
2023-08-30 20:02:29 +0900

    ボタンのクリックで、カレンダーの表示、非表示を実装

--- front/app/schedule/schedule-calender.tsx
+++ front/app/schedule/schedule-calender.tsx
@@ -6,21 +6,42 @@ import { Box, Input, Button, ChakraProvider } from '@chakra-ui/react';
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
+type CalendarStatus = 'hidden' | 'display';
 
 export const ScheduleCalender = () => {
   const [value, onChange] = useState<Value>(new Date());
+  const [calenderStatus, setCalenderStatus] = useState<CalendarStatus>('hidden');
 
-  return (
-    <ChakraProvider>
-      <Box>
-        <Input variant='outline' placeholder='開始日を選択してください' />
-        <Button>開始日</Button>
-        <Calendar
+  // カレンダーの表示・非表示を切り替える
+  const onChangeDate = () => {
+    if (calenderStatus === 'hidden') {
+      setCalenderStatus('display');
+    } else {
+      setCalenderStatus('hidden');
+    }
+  }
+
+  const calendar = () => {
+    if (calenderStatus === 'hidden') {
+      return <></>;
+    } else {
+      return <Calendar
         onChange={onChange}
         value={value}
         calendarType='hebrew'
-          />
-      </Box>
+      />
+    }
+  };
+
+  return (
+    <ChakraProvider>
+        <Box>
+          <Input variant='outline' placeholder='開始日を選択してください' />
+          <Button
+            onClick={onChangeDate}
+          >開始日</Button>
+          {calendar()}
+        </Box>
     </ChakraProvider>
   );
 }
