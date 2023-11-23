3250b74e71b0618c0ce05f375b198c1931256c75
tsuchida_nt <mamameto3071@gmail.com>
2023-11-23 10:26:30 +0000

    doc追加

--- front/.storybook/main.ts
+++ front/.storybook/main.ts
@@ -1,16 +1,12 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import path from "path";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../app/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../app/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm",
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -29,7 +25,6 @@ const config: StorybookConfig = {
           ...baseConfig.resolve?.alias,
           "@app": path.resolve(__dirname, "../app/"),
           "@lib": path.resolve(__dirname, "../library/"),
-          "@globalMenu": path.resolve(__dirname, "../app/_global-menu/"),
           "@": path.resolve(__dirname, "../"),
         },
       },
