11b8b23375cfd60ec1491f8492bf2781b22236ff
tsuchida_nt <mamameto3071@gmail.com>
2024-01-21 11:07:30 +0900

    タイトル変更

--- front/src/main-contents.tsx
+++ front/src/main-contents.tsx
@@ -81,7 +81,7 @@ export const MainContents = () => {
     <Grid placeContent="center" w={"100vw"} style={{ backgroundColor: 'white' }}>
       <Box>
         <Heading as="h1" size="xl" alignSelf="center">
-          VTuberWorld Scheduler
+          VTuber's Stream Scheduler
         </Heading>
       </Box>
       {
