10a40f0cd3a69a11044ffe8466975aab931a2d14
tsuchida_nt <mamameto3071@gmail.com>
2024-01-22 07:10:53 +0900

    twittercardの設定

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -4,14 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-          <meta name="theme-color" content="#ffffff" />
+        <title>Browser title</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content="VTuber&apos;s Weekly Stream Time Converter" />
+        <meta property="twitter:site" content="@ma_me" />
+        <meta property="twitter:description" content="A schedule creation app for VTubers. Set your timezone and automatically calculate broadcast times for fans around the world. complicated calculations! Create the perfect schedule with a few click" />
+        <meta property="og:image" content="https://weekly-stream-time-converter.vercel.app/twic.png" />
       </Head>
 
       <body>
